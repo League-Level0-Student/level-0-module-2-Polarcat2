2fcd0d64ebb532814330502d4655494baa50ecb5
student <student@ip-172-31-62-61.us-west-2.compute.internal>
2021-02-10 16:52:00 -0800

    DiceRoll.java

--- src/_01_random/_1_intro_to_random/IntroToRandom.java
+++ src/_01_random/_1_intro_to_random/IntroToRandom.java
@@ -15,11 +15,11 @@ public class IntroToRandom {
 		Random ran = new Random();
 		
 		/*************  Random integers with no bounds  *************/
-		
+
 		// Run this program and you will see that nextInt created 5 random integers which have been printed to the console
 		for (int i=0; i<5; i++) {
 			number = ran.nextInt();
-			System.out.println(number);
+//			System.out.println(number);
 		}
 		// The range of numbers that an int variable can hold is very large (as you can see in the console).
 		// Integers can also be positive or negative.
@@ -30,8 +30,8 @@ public class IntroToRandom {
 		for (int i=0; i<5; i++) {
 			// Uncomment the line of code below, but replace the _____ with a positive integer (e.g. 100) before running the program			
 
-			 number = ran.nextInt(56);
-			System.out.println(number);
+			 number = ran.nextInt(20);
+		//	System.out.println(number);
 			
 		}
 		
@@ -50,7 +50,7 @@ public class IntroToRandom {
 			// Also replace <lowest value> with the start of the range.  For our example, this is 25.
 			
 			 number = ran.nextInt(75 - 25 + 1) + 25;
-			System.out.println(number);
+		//	System.out.println(number);
 			
 		}
 		// This time, all the values printed should be between 25 and 75.
@@ -59,7 +59,10 @@ public class IntroToRandom {
 		/*************  Random integers with an upper and lower (negative) limit  *************/
 		
 		// Using the above example, generate 5 random numbers between -222 and 88
-
-
+		 for (int i=0; i<5; i++) {
+			 
+			 number = ran.nextInt(88 - -222 + 1) + -222;
+			 System.out.println(number);
+		 }
 	}
 }
